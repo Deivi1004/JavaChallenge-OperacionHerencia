686f5d62fd0f98bc4208054c02e0904b8ab7ff8a
Deivi10040 <josd.rojas@duocuc.cl>
2026-09-15 01:37:06 -0300

    Una familia de dispositivos

--- Main.java
+++ Main.java
@@ -4,5 +4,16 @@ public class Main {
 
         dispositivo.mostrarEstado();
         dispositivo.ejecutarDiagnostico();
+
+
+
+        Proyector proyector = new Proyector("Proyector Sala 201", true);
+        Computador computador = new Computador("PC Laboratorio", true);
+        Impresora impresora = new Impresora("Impresora Biblioteca", false);
+
+
+        proyector.mostrarEstado();
+        computador.mostrarEstado();
+        impresora.mostrarEstado();
     }
 }
